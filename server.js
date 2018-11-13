88610c1a23b1893fc372e382b4d67517f398f690
CuHTeTuk <cuhtetuk@CuHTeTuks-MBP.attlocal.net>
2018-11-12 20:41:53 -0600

    connection

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 
 var express = require("express");
-// var dotenv = require('dotenv')
+var dotenv = require('dotenv')
 var bodyParser = require("body-parser");
 var path = require("path");
 var mongoose = require("mongoose");
 
 
-// dotenv.config()
+dotenv.config()
 var db = require("./models");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3349;
+var PORT = process.env.PORT || 3000;
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -24,31 +24,10 @@ app.use(express.static("public"));
 //routing to our API folder with all the routes get and post 
 require("./routes/apiRoutes.js")(app);
 
-
-
-// localhost if we don't find one.
-var uristring =
-process.env.MONGOLAB_URI ||
-process.env.MONGOHQ_URL ||
-'mongodb://localhost/HelloMongoose';
-
-    // The http server will listen to an appropriate port, or default to
-    // port 5000.
-    var theport = process.env.PORT || 5000;
-
-
-    mongoose.connect(uristring, function (err, res) {
-      if (err) {
-      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-      } else {
-      console.log ('Succeeded connected to: ' + uristring);
-      }
-    });
-
 //connection to our mongo server 
-//  mongoose.connect("mongodb://localhost/forALEXA");
+mongoose.connect("mongodb://alexa:pass1word@ds159993.mlab.com:59993/heroku_jbjk6mn2");
 
 
-app.listen(theport, function() {
-    console.log("App running on port " + theport + "!");
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
   });
