bbbe997afb3a71c03c55d5dccac2b8d46b9adb24
CuHTeTuk <cuhtetuk@CuHTeTuks-MBP.attlocal.net>
2018-11-12 19:56:33 -0600

    mongo connection

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    // development: {
+    //     db: 'mongodb://localhost/mean-dev',
+
+    //     app: {
+    //         name: 'MEAN - A Modern Stack - Development'
+    //     }
+
+    // },
+    // test: {
+    //     db: 'mongodb://localhost/mean-test',
+
+    //     app: {
+    //         name: 'MEAN - A Modern Stack - Test'
+    //     }
+
+    // },
+    production: {
+        db: 'mongodb://<alexa>:<pass1word>@ds159993.mlab.com:59993/heroku_jbjk6mn2',
+       
+        app: {
+            name: 'MEAN - A Modern Stack - Production'
+        }
+
+    }
+};
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 
 var express = require("express");
-// var dotenv = require('dotenv')
+ var dotenv = require('dotenv')
 var bodyParser = require("body-parser");
 var path = require("path");
-var mongodb = require("mongoose");
+var mongoose = require("mongoose");
 
 
-// dotenv.config();
+
 var db = require("./models");
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3349;
 
+
+// dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -24,32 +26,41 @@ app.use(express.static("public"));
 //routing to our API folder with all the routes get and post 
 require("./routes/apiRoutes.js")(app);
 
-var url = process.env.MONGOLAB_URI;
+
+
+var env = process.env.NODE_ENV || 'production',
+    config = require('./config')[env],
+    mongoose = require('mongoose');
+
+var db = mongoose.connect(config.db);
+
+
+// var url = process.env.MONGOLAB_URI;
 
 
 //connection to our mongo server 
 //  mongoose.connect("mongodb://localhost/forALEXA");
 
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
+// var MongoClient = mongoose.MongoClient;
 
 // Connection URL. This is where your mongodb server is running.
 
 //(Focus on This Variable)
-var url = 'mongodb://localhost:27017/my_database_name';      
+// var url = 'mongodb://localhost:27017/my_database_name';      
 //(Focus on This Variable)
 
 // Use connect method to connect to the Server
-  MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
-    // do some work here with the database.
-    //Close connection
-    db.close();
-  }
-});
+//   MongoClient.connect(url, function (err, db) {
+//   if (err) {
+//     console.log('Unable to connect to the mongoDB server. Error:', err);
+//   } else {
+//     console.log('Connection established to', url);
+//     // do some work here with the database.
+//     //Close connection
+//     db.close();
+//   }
+// });
 
 
 app.listen(PORT, function() {
